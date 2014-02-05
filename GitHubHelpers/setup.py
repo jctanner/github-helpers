b9a0af32956528502042b8973f5cc1566af8869e
James Tanner <tanner.jc@gmail.com>
2014-02-05 17:09:16 -0500

    Include report script with setup.py

--- GitHubHelpers/setup.py
+++ GitHubHelpers/setup.py
@@ -4,7 +4,7 @@ setup(
     name='GitHubHelpers',
     version='0.1',
     packages=['github',],
-    scripts=['bin/triage','bin/issues'],
+    scripts=['bin/triage','bin/issues', 'bin/runreports'],
     license='Apache',
     long_description="Github issue api helpers",
 )
