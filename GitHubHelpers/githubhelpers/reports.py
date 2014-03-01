5544f992e6a0bd0d165f4ed3f153680a66d471e9
James Tanner <tanner.jc@gmail.com>
2014-03-01 10:10:06 -0500

    Initial work on pandas graphing

--- GitHubHelpers/githubhelpers/reports.py
+++ GitHubHelpers/githubhelpers/reports.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import os
+import shutil
+import tempfile
 import epdb
 import cPickle as pickle
 from github import Github
@@ -9,6 +11,24 @@ from datetime import datetime as DT
 import datetime
 from prtests import PRTest
 from htmlify import HtmlGenerator
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+time_dict = { 'total_opened': 0,
+              'total_closed': 0,
+              'total_open': 0,
+              'closed_by_user': 0,
+              'closed_by_admin': 0,
+              'prs_opened': 0,
+              'prs_closed': 0,
+              'prs_closed_by_user': 0,
+              'prs_closed_by_admin': 0,
+              'issues_opened': 0,
+              'issues_closed': 0,
+              'issues_closed_by_user': 0,
+              'issues_closed_by_admin': 0
+            }
 
 class TicketRates(object):
     def __init__(self, cli=None):
@@ -22,10 +42,11 @@ class TicketRates(object):
 
         self.find_date_ranges()
         self.make_time_series()
-        self.count_open_and_closed()
         self.load_pygithub_objects()
         self.closed_by()
-        self.show_csv()
+        self.count_open_and_closed()
+        self.create_csv()
+        self.plot_csv()
 
         #self.closed_by()
 
@@ -56,69 +77,89 @@ class TicketRates(object):
     def count_open_and_closed(self):
 
         for i in self.gh.datadict.keys():
+            created_date = self.gh.datadict[i]['created_at']
+            od = DT.strptime(created_date, "%Y-%m-%dT%H:%M:%SZ")
+            od_str = od.strftime('%Y-%m-%d')
+            if od_str not in self.time_series:
+                self.time_series[od_str] = time_dict.copy()
+
+            if 'closed_at' in self.gh.datadict[i] :
+                if self.gh.datadict[i]['closed_at']:
+                    closed_date = self.gh.datadict[i]['closed_at']
+                    cd = DT.strptime(closed_date, "%Y-%m-%dT%H:%M:%SZ")
+                    cd_str = cd.strftime('%Y-%m-%d')
+                    if cd_str not in self.time_series:
+                        self.time_series[cd_str] = time_dict.copy()
+
+        for i in self.gh.datadict.keys():
 
             created_date = None
             closed_date = None
             od = None
             cd = None
 
+            ## OPEN
             created_date = self.gh.datadict[i]['created_at']
             od = DT.strptime(created_date, "%Y-%m-%dT%H:%M:%SZ")
             od_str = od.strftime('%Y-%m-%d')
-            if od_str not in self.time_series:
-                self.time_series[od_str] = {}
-                self.time_series[od_str]['opened'] = 1
-            else:
-                if 'opened' not in self.time_series[od_str]:
-                    self.time_series[od_str]['opened'] = 1
-                else:
-                    self.time_series[od_str]['opened'] += 1
+            self.time_series[od_str]['total_opened'] += 1
 
+            if self.gh.datadict[i]['type'] == 'issue':
+                self.time_series[od_str]['issues_opened'] += 1
+            if self.gh.datadict[i]['type'] == 'pull_request':
+                self.time_series[od_str]['prs_opened'] += 1
+
+            ## CLOSED
             if self.gh.datadict[i]['closed_at']:
                 closed_date = self.gh.datadict[i]['closed_at']
                 cd = DT.strptime(closed_date, "%Y-%m-%dT%H:%M:%SZ")
                 cd_str = cd.strftime('%Y-%m-%d')
+                self.time_series[cd_str]['total_closed'] += 1
 
-                if cd_str not in self.time_series:
-                    self.time_series[cd_str] = {}
-                    self.time_series[cd_str]['closed'] = 1
+                if self.gh.datadict[i]['user_closed']:
+                    self.time_series[cd_str]['closed_by_user'] += 1
                 else:
-                    if 'closed' not in self.time_series[cd_str]:
-                        self.time_series[cd_str]['closed'] = 1
+                    self.time_series[cd_str]['closed_by_admin'] += 1
+
+                #if self.time_series[cd_str]['closed_by_admin'] > 100:
+                #    import epdb; epdb.st()
+
+                if self.gh.datadict[i]['type'] == 'issue':
+                    self.time_series[od_str]['issues_closed'] += 1
+                    if self.gh.datadict[i]['user_closed']:
+                        self.time_series[cd_str]['issues_closed_by_user'] += 1
                     else:
-                        self.time_series[cd_str]['closed'] += 1
+                        self.time_series[cd_str]['issues_closed_by_admin'] += 1
+                if self.gh.datadict[i]['type'] == 'pull_request':
+                    self.time_series[od_str]['prs_closed'] += 1
+                    if self.gh.datadict[i]['user_closed']:
+                        self.time_series[cd_str]['prs_closed_by_user'] += 1
+                    else:
+                        self.time_series[cd_str]['prs_closed_by_admin'] += 1
 
         total = 0
         for i in sorted(self.time_series.keys()):
-            if 'opened' in self.time_series[i]:
-                total += self.time_series[i]['opened']
+            total += self.time_series[i]['total_opened']
+            total -= self.time_series[i]['total_closed']
 
-            if 'closed' in self.time_series[i]:
-                total -= self.time_series[i]['closed']
+            self.time_series[i]['total_open'] = total
 
-            self.time_series[i]['open'] = total
+    def create_csv(self):
+        
+        all_keys = []
+        for k in self.time_series.keys():
+            for l in self.time_series[k].keys():
+                if l not in all_keys:
+                    all_keys.append(l)                
 
-
-    def show_csv(self):
-
-        print "date;opened;closed;total_open"
+        all_keys = sorted(all_keys)
+        self.csv = "date;" + ';'.join(all_keys) + "\n"
 
         for k in sorted(self.time_series.keys()):
-
-            if 'opened' in self.time_series[k]:
-                opened = self.time_series[k]['opened']
-            else:
-                opened = 0
-
-            if 'closed' in self.time_series[k]:
-                closed = self.time_series[k]['closed']
-            else:
-                closed = 0
-
-            total = self.time_series[k]['open']
-
-            #print k,";",opened,";",closed
-            print "%s;%d;%d;%d" % (k, opened, closed, total)
+            this_string = "%s" % k
+            for l in sorted(all_keys):
+                this_string += ";%d" % self.time_series[k][l] 
+            self.csv += this_string + "\n"
 
     def load_pygithub_objects(self):
 
@@ -165,6 +206,8 @@ class TicketRates(object):
         # this_repo.get_issue(1).user.login
         # this_repo.get_issue(1).closed_by.login
 
+        # self.repo.organization.get_members()
+
         for k in sorted(self.gh.datadict.keys()):
             this_issue = self.gh.datadict[k]['pygithub']
             this_creator = this_issue.user.login
@@ -181,7 +224,7 @@ class TicketRates(object):
             else:
                 self.gh.datadict[k]['user_closed'] = False
 
-            import epdb; epdb.st()
+            #import epdb; epdb.st()
 
 
     """
@@ -192,3 +235,62 @@ class TicketRates(object):
                 parse_dates=['date'], 
                 index_col='date')
     """
+
+    def plot_csv(self):
+
+        # known to work with pandas 0.10.0
+
+        this_file = tempfile.NamedTemporaryFile()
+        this_filename = this_file.name
+        this_file.write(self.csv)
+
+        print "# loading csv %s" % this_filename
+        df = pd.read_csv(this_filename, sep=';', parse_dates=['date'], index_col='date')
+        shutil.copyfile(this_filename, 
+            "/var/www/html/ansible/stats/open_closure_rates/latest-data.csv")
+        this_file.close()
+        #import epdb; epdb.st()
+
+        ################################
+        #           TOTALS
+        ################################
+        print "# making plot"
+        ax = df.plot(y=['total_closed','total_opened', 'total_open'], 
+                                legend=False, figsize=(30, 20), grid=True)
+        opened, closed = ax.get_legend_handles_labels()
+        ax.legend(opened, closed, loc='best')
+        fig = ax.get_figure()
+        fig.tight_layout()
+        print "# saving plot to file"
+        fig.savefig('/var/www/html/ansible/stats/open_closure_rates/cumulative-totals.png')
+
+
+        ################################
+        #       PULL vs. ISSUE 
+        ################################
+        print "# making plot"
+        nf = pd.DataFrame()
+        columns = ['issues_opened', 'issues_closed', 'prs_opened', 'prs_closed']
+
+        nf = nf + df.issues_opened
+        nf = nf + df.issues_closed
+        nf = nf + df.prs_opened
+        nf = nf + df.prs_closed
+
+        #ax = df.plot(y=columns, legend=False, figsize=(30, 20), grid=True)
+        #bx = df[-90:].plot(y=*columns, kind='bar', stacked=False, 
+        #                legend=False, figsize=(30, 20))
+        bx = nf[-90:].plot(kind='bar', stacked=False, legend=False, 
+                          figsize=(30, 20))
+        import epdb; epdb.st()
+        #opened, closed = bx.get_legend_handles_labels()
+        
+        #issues_opened, issues_closed, prs_opened, prs_closed = bx.get_legend_handles_labels()
+        #bx.legend(issues_opened, issues_closed, prs_opened, prs_closed, loc='best')
+        bx.legend(loc='best')
+        fig2 = bx.get_figure()
+        fig2.tight_layout()
+        print "# saving plot to file"
+        fig2.savefig('/var/www/html/ansible/stats/open_closure_rates/pr-vs-issue-graph.png')
+
+
