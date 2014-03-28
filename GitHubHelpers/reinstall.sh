0aec185d4c84e262abbab6bcb9897e1fef6d66cf
James Tanner <tanner.jc@gmail.com>
2014-03-28 14:27:04 -0400

    install -after- uninstall

--- GitHubHelpers/reinstall.sh
+++ GitHubHelpers/reinstall.sh
@@ -4,11 +4,11 @@ SITEDIR=$(python -c "from distutils.sysconfig import get_python_lib; print(get_p
 rm -rf $SITEDIR/githubhelpers
 rm -f $SITEDIR/GitHubHelpers-*.egg-info
 
-python setup.py install
-
 REPORTS=$(which runreports)
 rm -rf $REPORTS
 ISSUES=$(which issues)
 rm -rf $ISSUES
 TRIAGE=$(which triage)
 rm -rf $TRIAGE
+
+python setup.py install
