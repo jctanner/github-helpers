f95da7d99d747be2821b311be7c3fc25853fd2e3
James Tanner <tanner.jc@gmail.com>
2014-03-28 13:23:27 -0400

    Enumerate cli args first

--- GitHubHelpers/githubhelpers/pygithubwrapper.py
+++ GitHubHelpers/githubhelpers/pygithubwrapper.py
@@ -12,10 +12,15 @@ class PyGithubWrapper(object):
 
     def load_pygithub_objects(self, datadict):
 
-        # FIXME -- refactor to a new class
+        if self.cli.pargs.username is not None:
+            self.username = self.cli.pargs.username
+        else:
+            self.username = self.cli.config.get('github', 'username')
 
-        username = self.cli.config.get('github', 'username')
-        password = self.cli.config.get('github', 'password')
+        if self.cli.pargs.password is not None:
+            self.password = self.cli.pargs.password
+        else:
+            self.password = self.cli.config.get('github', 'password')
 
         if self.cli.pargs.repo is not None:
             repo_data = self.cli.pargs.repo
