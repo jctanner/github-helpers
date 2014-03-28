2c38782b1432623bace4cd1af08c9aa41fdf3db0
James Tanner <tanner.jc@gmail.com>
2014-03-28 14:26:38 -0400

    Add reinstall script

---
+++ GitHubHelpers/reinstall.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+SITEDIR=$(python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())")
+
+rm -rf $SITEDIR/githubhelpers
+rm -f $SITEDIR/GitHubHelpers-*.egg-info
+
+python setup.py install
+
+REPORTS=$(which runreports)
+rm -rf $REPORTS
+ISSUES=$(which issues)
+rm -rf $ISSUES
+TRIAGE=$(which triage)
+rm -rf $TRIAGE
