ed02e21f220f4c43b00618f261955450f9da6d81
James Tanner <tanner.jc@gmail.com>
2014-03-28 14:50:41 -0400

    Add example vagrant file and install playbook

---
+++ vagrant/reportscript.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+killall Xvfb
+export DISPLAY=:1 
+Xvfb $DISPLAY -auth /dev/null &
+
+ACTIVATE=$1
+USERNAME=$2
+PASSWORD=$3
+REPO=$4
+OUTPUTDIR=$5
+
+source $ACTIVATE 
+echo --username="$USERNAME" --password="$PASSWORD" --repo="$REPO" --outputdir="$OUTPUTDIR" >> /tmp/awx.log
+issues showrates --username="$USERNAME" --password="$PASSWORD" --repo="$REPO" --outputdir="$OUTPUTDIR"
+
+
+killall Xvfb
